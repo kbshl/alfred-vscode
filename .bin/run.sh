47e0964fa417046893ef1ec3f07d1275e18afa5e
asuforce <owata.sn@gmail.com>
2019-01-31 14:02:09 +0900

    Use vscode default cli command

--- .bin/run.sh
+++ .bin/run.sh
@@ -1,8 +1,1 @@
-VSCODE_APP_EDITION="Visual Studio Code"
-
-if [ $vscodeEdition == 'code-insiders' ]
-then
-  VSCODE_APP_EDITION="Visual Studio Code - Insiders"
-fi
-
-open -a "${VSCODE_APP_EDITION}" "$1"
+"${vscodeEdition}" "$1"
