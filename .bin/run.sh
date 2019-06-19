1a4afe12758e5e288a95e977f3c90d1bfd5b35b5
asuforce <owata.sn@gmail.com>
2019-06-19 15:17:19 +0900

    Format

--- .bin/run.sh
+++ .bin/run.sh
@@ -3,17 +3,17 @@ VSCODE_APP_EDITION="Visual Studio Code"
 
 if [ $vscodeEdition == 'code-insiders' ]
 then
-    VSCODE_APP_EDITION="Visual Studio Code - Insiders"
+  VSCODE_APP_EDITION="Visual Studio Code - Insiders"
 fi
 
 if [ $vscodeEdition == 'codium' ]
 then
-    VSCODE_APP_EDITION="VSCodium"
+  VSCODE_APP_EDITION="VSCodium"
 fi
 
 if [ $reuseWindow == 'true' ]
 then
-    REUSE_WINDOW="-r"
+  REUSE_WINDOW="-r"
 fi
 
 open -a "${VSCODE_APP_EDITION}" "${REUSE_WINDOW}" "$1"
